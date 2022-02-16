1d00399854687e37db8243634fea439d739307a4
Oskar Zając <o.zajac@stud.elka.pw.edu.pl>
2022-02-16 16:25:27 +0100

    Added search textfield and DropdownList (front)

---
+++ lib/search.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class search extends StatelessWidget {
+  const search({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 10),
+      child: TextField(
+        maxLength: 60,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+            counterText: '',
+            suffixIcon: Padding(
+              padding: EdgeInsetsDirectional.only(end: 5),
+              child: Icon(Icons.search),
+            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            filled: true,
+            hintStyle: TextStyle(color: Colors.grey),
+            hintText: '',
+            fillColor: Colors.white70),
+      ),
+    );
+  }
+}
