4415bbb53b5997a82fe036f864f63a3a7863b5b0
Oskar Zając <o.zajac@stud.elka.pw.edu.pl>
2022-02-18 12:16:34 +0100

    getting country from API, unpacking it

---
+++ lib/getData.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Country> fetchAlbum(
+    {String link = 'https://restcountries.com/v3.1/name/poland)'}) async {
+  var response = await http.get(Uri.parse(link));
+
+  if (response.statusCode == 200) {
+    var decodedResponse =
+        jsonDecode(utf8.decode(response.bodyBytes)) as List<dynamic>;
+    return Country(
+        name: decodedResponse[0]['name']['common'],
+        capital: decodedResponse[0]['capital'][0],
+        population: decodedResponse[0]['population'],
+        area: decodedResponse[0]['area'],
+        currency: decodedResponse[0]['currencies'].values.elementAt(0)['name'],
+        subregion: decodedResponse[0]['subregion']);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Country');
+  }
+}
+
+class Country {
+  String name;
+  final String capital;
+  final int population;
+  final double area;
+  final String currency;
+  final String subregion;
+
+  Country(
+      {required this.name,
+      required this.capital,
+      required this.population,
+      required this.area,
+      required this.currency,
+      required this.subregion});
+
+  // factory Country.fromJson(Map<String, dynamic> json) {
+  //   return Country(name: json['name'], capital: json['capital'], population: json['population']);
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './search.dart';
+import './getData.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,11 +37,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _items = ['By name', 'By continent', 'By currency', 'By capital'];
   var _selectedValue = 'By name';
+  int flag = 0;
 
   void _pickList(String? newValue) {
     setState(() {
       _selectedValue = newValue!;
     });
+  }
+
+  void _countryView() {
+    setState(() {
+      flag = 1;
+    });
+  }
+
+  void initState() {
+    super.initState();
+    var country = fetchAlbum();
   }
 
   @override
@@ -49,30 +62,64 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Column(
-          children: [
-            search(),
-            DropdownButton<String>(
-              value: _selectedValue,
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurple,
-              ),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedValue = newValue!;
-                });
-              },
-              items: _items.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
+        body: flag == 0
+            ? Column(
+                children: [
+                  search(),
+                  newMethod(),
+                  FloatingActionButton(
+                      onPressed: _countryView,
+                      child: Icon(Icons.arrow_forward_rounded))
+                ],
+              )
+            : Column(
+                children: [
+                  FutureBuilder<Country>(
+                      future: fetchAlbum(),
+                      builder: (context, snapshot) {
+                        return snapshot.hasData
+                            ? Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.all(10),
+                                    child: Row(
+                                      children: [
+                                        FloatingActionButton(
+                                          onPressed: null,
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )
+                            : Center(
+                                child: CircularProgressIndicator(),
+                              );
+                      })
+                ],
+              ));
+  }
+
+  DropdownButton<String> newMethod() {
+    return DropdownButton<String>(
+                  value: _selectedValue,
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurple,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedValue = newValue!;
+                    });
+                  },
+                  items: _items.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 );
-              }).toList(),
-            )
-          ],
-        ));
   }
 }
